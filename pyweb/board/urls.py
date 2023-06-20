94d0f69d83fda1fa6e616145f1eee8ef63d90e0a
mynameiseuiseok <sunges1014@naver.com>
2023-06-20 11:18:11 +0900

    form register

--- pyweb/board/urls.py
+++ pyweb/board/urls.py
@@ -6,4 +6,8 @@ app_name = 'board'
 urlpatterns = [
     path('', views.question_list, name='question_list'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('question/create/', views.question_create,
+         name='question_create'),
+    path('answer/create/<int:question_id>/', views.answer_create,
+         name='answer_create')
 ]
--- pyweb/board/views.py
+++ pyweb/board/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from board.models import Question
+from board.forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -16,4 +18,35 @@ def question_list(request):
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     context = {'question': question}
+    return render(request, 'board/detail.html', context)
+
+# 질문 등록
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)   # 입력된 데이터가 있는 폼
+        if form.is_valid(): # 폼이 유효성 검사를 통과했다면
+            question = form.save(commit=False) # 가저장
+            question.create_date = timezone.now()   # 등록일 생성
+            form.save()
+            return redirect('board:question_list')  # 질문 목록 페이지 이동
+    else:   # get 방식
+        form = QuestionForm()   # 폼 객체 생성(빈 폼 생성)
+    context = {'form': form}
+    return render(request, 'board/question_form.html', context)  # get 방식
+
+# 답변 등록
+def answer_create(request, question_id):
+    # 질문이 1개 있어야 답변을 등록할 수 있음
+    question = Question.objects.get(id=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)    # content만 저장
+            answer.create_date = timezone.now() # 답변 등록일
+            answer.question = question          # 외래키 생성
+            form.save()
+            return redirect('board:detail', question_id=question.id) # question.id 주의!!
+    else:
+        form = AnswerForm()  # 빈 폼 생성
+    context = {'question': question, 'form': form}
     return render(request, 'board/detail.html', context)
