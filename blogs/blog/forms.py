96d423c812a321998738e1504972633b479583bc
mynameiseuiseok <sunges1014@naver.com>
2023-06-26 15:55:51 +0900

    20230626

--- blogs/blog/forms.py
+++ blogs/blog/forms.py
@@ -5,10 +5,11 @@ from blog.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post    # Post 객체 생성
-        fields = ['title', 'content', 'photo', 'category']
+        fields = ['title', 'content', 'photo', 'file', 'category']
         labels = {
             'title': '제목',
             'content': '내용',
             'photo': '사진',
+            'file': '파일',
             'category': '분류'
         }
--- blogs/blog/models.py
+++ blogs/blog/models.py
@@ -1,5 +1,9 @@
+import os
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+
 
 # 카테고리 모델
 class Category(models.Model):
@@ -14,7 +18,9 @@ class Category(models.Model):
 
     # 카테고리 url 주소
     def get_absolute_url(self):
-        return f'/blog/category/{self.slug}'
+        # return f'/blog/category/{self.slug}'  # 절대 경로
+        # reverse() - redirect 유사 : app-name으로 경로 이동
+        return reverse('blog:category_page', args=[self.slug])
 
     # 관리자 페이지에서 적용 - verbose_name_plural(복수형)
     class Meta:
@@ -31,9 +37,20 @@ class Post(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)   # 입력 폼이 비어도 됨
     photo = models.ImageField(upload_to='blog/images/%Y/%m/%d/',
                     null=True, blank=True)  # null 허용, 파일을 첨부하지 않을 수 있음
+    file = models.FileField(upload_to='blog/files/%Y/%m/%d/',
+                    null=True, blank=True)
     # models.SET_NULL: 카테고리가 삭제되어도 카테고리가 없는 포스트는 유지
     category = models.ForeignKey(Category, null=True, blank=True,
                     on_delete=models.SET_NULL)  # 카테고리가 삭제되어도
 
     def __str__(self):
         return self.title
+
+    # 파일의 이름 출력
+    def get_file_name(self):
+        return os.path.basename(self.file.name)
+
+    # 파일의 확장자 구분
+    def get_file_ext(self):
+        # seoul.csv -> split() -> [seoul, csv]
+        return self.get_file_name().split('.')[-1]
